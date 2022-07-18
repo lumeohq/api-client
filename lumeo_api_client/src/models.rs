7041e392b77558c2e1cf7d01e23a1ce41ef02403
Max Bruckner <max@lumeo.com>
2022-07-18 14:18:32 +0200

    Fix color mode and input order serialization and add tests

--- lumeo_api_client/src/models.rs
+++ lumeo_api_client/src/models.rs
@@ -51,7 +51,7 @@ pub struct ModelInferenceConfig {
     pub classifier_threshold: Option<f64>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "lowercase")]
 pub enum Capability {
     Classification,
@@ -61,7 +61,7 @@ pub enum Capability {
     Other,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "lowercase")]
 pub enum Architecture {
     DetectNet,
@@ -83,7 +83,7 @@ pub enum Architecture {
     YoloV4Tiny,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "lowercase")]
 pub enum Format {
     Caffe,
@@ -95,15 +95,17 @@ pub enum Format {
     OpenVino,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Copy, Debug, Deserialize, Serialize, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub enum ModelColorFormat {
-    RGB,
-    BGR,
+    #[serde(alias = "r_g_b")] // TODO: Remove once not used anymore
+    Rgb,
+    #[serde(alias = "b_g_r")] // TODO: Remove once not used anymore
+    Bgr,
     Grayscale,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Copy, Debug, Deserialize, Serialize, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub enum ModelNetworkMode {
     Float32,
@@ -111,12 +113,15 @@ pub enum ModelNetworkMode {
     Float16,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Copy, Debug, Deserialize, Serialize, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub enum ModelInputOrder {
-    NCHW,
-    NHWC,
-    NC,
+    #[serde(alias = "n_c_h_w")] // TODO: Remove once not used anymore
+    Nchw,
+    #[serde(alias = "n_h_w_c")] // TODO: Remove once not used anymore
+    Nhwc,
+    #[serde(alias = "n_c")] // TODO: Remove once not used anymore
+    Nc,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -138,7 +143,7 @@ pub struct ModelClassAttributes {
     pub top_k: Option<i32>,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Clone, Copy, Debug, Deserialize, Serialize, PartialEq, Eq)]
 #[serde(rename_all = "snake_case")]
 pub enum ClusterMode {
     OpenCvGroupRectangles,
@@ -161,10 +166,173 @@ impl Client {
 
 #[cfg(test)]
 mod tests {
+    use serde::de::DeserializeOwned;
+    use serde_json::{json, Value as JsonValue};
 
-    use serde_json::json;
+    use super::*;
 
-    use super::Model;
+    #[test]
+    fn should_deserialize_capability() {
+        use Capability::*;
+        assert_eq!(Classification, deserialize_json(json!("classification")));
+        assert_eq!(Detection, deserialize_json(json!("detection")));
+        assert_eq!(Segmentation, deserialize_json(json!("segmentation")));
+        assert_eq!(Transformation, deserialize_json(json!("transformation")));
+        assert_eq!(Other, deserialize_json(json!("other")));
+    }
+
+    #[test]
+    fn should_serialize_capability() {
+        use Capability::*;
+        assert_eq!(json!("classification"), serialize_json(Classification));
+        assert_eq!(json!("detection"), serialize_json(Detection));
+        assert_eq!(json!("segmentation"), serialize_json(Segmentation));
+        assert_eq!(json!("transformation"), serialize_json(Transformation));
+        assert_eq!(json!("other"), serialize_json(Other));
+    }
+
+    #[test]
+    fn should_deserialize_architecture() {
+        use Architecture::*;
+        assert_eq!(DetectNet, deserialize_json(json!("detectnet")));
+        assert_eq!(Frcnn, deserialize_json(json!("frcnn")));
+        assert_eq!(MobileNet, deserialize_json(json!("mobilenet")));
+        assert_eq!(ResNet, deserialize_json(json!("resnet")));
+        assert_eq!(Ssd, deserialize_json(json!("ssd")));
+        assert_eq!(TinyYolo, deserialize_json(json!("tinyyolo")));
+        assert_eq!(Yolo, deserialize_json(json!("yolo")));
+        assert_eq!(YoloV2, deserialize_json(json!("yolov2")));
+        assert_eq!(YoloV3, deserialize_json(json!("yolov3")));
+        assert_eq!(YoloV4, deserialize_json(json!("yolov4")));
+        assert_eq!(YoloV5, deserialize_json(json!("yolov5")));
+        assert_eq!(YoloV2Tiny, deserialize_json(json!("yolov2_tiny")));
+        assert_eq!(YoloV3Tiny, deserialize_json(json!("yolov3_tiny")));
+        assert_eq!(YoloV4Tiny, deserialize_json(json!("yolov4_tiny")));
+    }
+
+    #[test]
+    fn should_serialize_architecture() {
+        use Architecture::*;
+        assert_eq!(json!("detectnet"), serialize_json(DetectNet));
+        assert_eq!(json!("frcnn"), serialize_json(Frcnn));
+        assert_eq!(json!("mobilenet"), serialize_json(MobileNet));
+        assert_eq!(json!("resnet"), serialize_json(ResNet));
+        assert_eq!(json!("ssd"), serialize_json(Ssd));
+        assert_eq!(json!("tinyyolo"), serialize_json(TinyYolo));
+        assert_eq!(json!("yolo"), serialize_json(Yolo));
+        assert_eq!(json!("yolov2"), serialize_json(YoloV2));
+        assert_eq!(json!("yolov3"), serialize_json(YoloV3));
+        assert_eq!(json!("yolov4"), serialize_json(YoloV4));
+        assert_eq!(json!("yolov5"), serialize_json(YoloV5));
+        assert_eq!(json!("yolov2_tiny"), serialize_json(YoloV2Tiny));
+        assert_eq!(json!("yolov3_tiny"), serialize_json(YoloV3Tiny));
+        assert_eq!(json!("yolov4_tiny"), serialize_json(YoloV4Tiny));
+    }
+
+    #[test]
+    fn should_deserialize_format() {
+        use Format::*;
+        assert_eq!(Caffe, deserialize_json(json!("caffe")));
+        assert_eq!(Etlt, deserialize_json(json!("etlt")));
+        assert_eq!(YoloNative, deserialize_json(json!("yolonative")));
+        assert_eq!(Onnx, deserialize_json(json!("onnx")));
+        assert_eq!(Uff, deserialize_json(json!("uff")));
+        assert_eq!(TensorFlow, deserialize_json(json!("tensorflow")));
+        assert_eq!(OpenVino, deserialize_json(json!("openvino")));
+    }
+
+    #[test]
+    fn should_serialize_format() {
+        use Format::*;
+        assert_eq!(json!("caffe"), serialize_json(Caffe));
+        assert_eq!(json!("etlt"), serialize_json(Etlt));
+        assert_eq!(json!("yolonative"), serialize_json(YoloNative));
+        assert_eq!(json!("onnx"), serialize_json(Onnx));
+        assert_eq!(json!("uff"), serialize_json(Uff));
+        assert_eq!(json!("tensorflow"), serialize_json(TensorFlow));
+        assert_eq!(json!("openvino"), serialize_json(OpenVino));
+    }
+
+    #[test]
+    fn should_deserialize_color_format() {
+        use ModelColorFormat::*;
+        assert_eq!(Rgb, deserialize_json(json!("rgb")));
+        assert_eq!(Rgb, deserialize_json(json!("r_g_b"))); // TODO: Remove once not used anymore
+        assert_eq!(Bgr, deserialize_json(json!("bgr")));
+        assert_eq!(Bgr, deserialize_json(json!("b_g_r"))); // TODO: Remove once not used anymore
+        assert_eq!(Grayscale, deserialize_json(json!("grayscale")));
+    }
+
+    #[test]
+    fn should_serialize_color_format() {
+        use ModelColorFormat::*;
+        assert_eq!(json!("rgb"), serialize_json(Rgb));
+        assert_eq!(json!("bgr"), serialize_json(Bgr));
+        assert_eq!(json!("grayscale"), serialize_json(Grayscale));
+    }
+
+    #[test]
+    fn should_deserialize_network_mode() {
+        use ModelNetworkMode::*;
+        assert_eq!(Float32, deserialize_json(json!("float32")));
+        assert_eq!(Int8, deserialize_json(json!("int8")));
+        assert_eq!(Float16, deserialize_json(json!("float16")));
+    }
+
+    #[test]
+    fn should_serialize_network_mode() {
+        use ModelNetworkMode::*;
+        assert_eq!(json!("float32"), serialize_json(Float32));
+        assert_eq!(json!("int8"), serialize_json(Int8));
+        assert_eq!(json!("float16"), serialize_json(Float16));
+    }
+
+    #[test]
+    fn should_deserialize_input_order() {
+        use ModelInputOrder::*;
+        assert_eq!(Nchw, deserialize_json(json!("nchw")));
+        assert_eq!(Nchw, deserialize_json(json!("n_c_h_w"))); // TODO: Remove once not used anymore
+        assert_eq!(Nhwc, deserialize_json(json!("nhwc")));
+        assert_eq!(Nhwc, deserialize_json(json!("n_h_w_c"))); // TODO: Remove once not used anymore
+        assert_eq!(Nc, deserialize_json(json!("nc")));
+        assert_eq!(Nc, deserialize_json(json!("n_c"))); // TODO: Remove once not used anymore
+    }
+
+    #[test]
+    fn should_serialize_input_order() {
+        use ModelInputOrder::*;
+        assert_eq!(json!("nchw"), serialize_json(Nchw));
+        assert_eq!(json!("nhwc"), serialize_json(Nhwc));
+        assert_eq!(json!("nc"), serialize_json(Nc));
+    }
+
+    #[test]
+    fn should_deserialize_cluster_mode() {
+        use ClusterMode::*;
+        assert_eq!(OpenCvGroupRectangles, deserialize_json(json!("open_cv_group_rectangles")));
+        assert_eq!(Dbscan, deserialize_json(json!("dbscan")));
+        assert_eq!(Nms, deserialize_json(json!("nms")));
+        assert_eq!(DbscanNmsHybrid, deserialize_json(json!("dbscan_nms_hybrid")));
+        assert_eq!(NoClustering, deserialize_json(json!("no_clustering")));
+    }
+
+    #[test]
+    fn should_serialize_cluster_mode() {
+        use ClusterMode::*;
+        assert_eq!(json!("open_cv_group_rectangles"), serialize_json(OpenCvGroupRectangles));
+        assert_eq!(json!("dbscan"), serialize_json(Dbscan));
+        assert_eq!(json!("nms"), serialize_json(Nms));
+        assert_eq!(json!("dbscan_nms_hybrid"), serialize_json(DbscanNmsHybrid));
+        assert_eq!(json!("no_clustering"), serialize_json(NoClustering));
+    }
+
+    fn deserialize_json<T: DeserializeOwned>(json: JsonValue) -> T {
+        serde_json::from_value(json).expect("Failed to deserialize JSON")
+    }
+
+    fn serialize_json<T: Serialize>(value: T) -> JsonValue {
+        serde_json::to_value(value).expect("Failed to serialize JSON")
+    }
 
     #[test]
     fn try_to_deserialize_model() {
@@ -184,10 +352,10 @@ mod tests {
             "gallery_img_url": null,
             "inference_config": {
                 "net_scale_factor": 3.25_f64,
-                "color_format": "r_g_b",
+                "color_format": "rgb",
                 "network_mode": "float32",
                 "infer_dims": "dims",
-                "input_order": "n_c_h_w",
+                "input_order": "nchw",
                 "input_blob_name": "blob_name",
                 "output_blob_names": ["a","b","c"],
                 "tlt_model_key": "model_key",
